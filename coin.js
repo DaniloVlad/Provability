768f199cfd59b32db8e20b330e4830768af52a78
danilovlad <danilo.vladicic@gmail.com>
2019-03-11 18:24:45 -0400

    Much cleaner

--- coin.js
+++ coin.js
@@ -2,36 +2,28 @@ const crypto = require("crypto");
 
 const HEADS = 0
 const TAILS = 1
-const MAXBETS = 4 //
+const MAXBETS = 10 // 256 bits/bet-string / 20 bits/bet
 
-const aes256 = (key, salt, iv) => {
+const aes256 = (seed, key, iv) => {
   //randomize data a bit more
   //dont rely on the company for true random number generation. 
   //Prevent pre-image attacks
-  let cipher = crypto.createCipher("aes256", salt);
-  if(iv) 
-    cipher = crypto.createCipheriv("aes256", salt, iv);
-
-  return cipher.update(key).toString("hex")
+  let cipher = crypto.createCipheriv("aes256", key, iv);
+  return cipher.update(seed).toString("hex")
 }
 
 //returns random 256 bit string
-const genServerSeed = (callback) => {
-  var seed;
-  if(callback) {
-    crypto.randomBytes(32, callback)
-  } 
-  else {
-    seed = crypto.randomBytes(32);
-    return seed
-  }
+const genServerSeed = () => {
+  var seed = crypto.randomBytes(32);
+  return seed
 }
 
-const genBetSig = (key, salt, iv) => {
+const genBetSig = (seed, key, iv, salt) => {
   let cipher, sig, bets, hmac;
-  cipher = aes256(key, salt, iv);
+  cipher = aes256(seed, key, iv);
   hmac = crypto.createHmac("sha256", salt);
-  bets = hmac.update(cipher).digest('hex');
+  bets = hmac.update(cipher).digest("hex");
+  //here no salt should be used
   sig = crypto.createHash("sha256").update(bets).digest("hex");
 
   return {bets, sig}
@@ -40,8 +32,8 @@ const genBetSig = (key, salt, iv) => {
 const placeBet = (bet, bstring) => {
   //bet logic here
   //use betstring to generate roll
-  const roll = parseInt(bstring,16);
-  const scale = ((100 * roll) / (Math.pow(2, 20)-1)); //scale to [0, 100]
+  const roll = parseInt(bstring, 16);
+  const scale = ((100 * roll) / (Math.pow(2, 20) - 1)); //scale to [0, 100]
   return { 
     bet: bet == HEADS ? scale < 49.50 : scale > 50.50, 
     roll: scale, 
@@ -50,32 +42,36 @@ const placeBet = (bet, bstring) => {
 
 }
 
-const verify = (serverSeed, salt, iv, signature) => {
-  console.log("Verifying:"+
-    "\n\tServer Seed: "+serverSeed.toString('hex')
-    +"\n\tSalt: "+salt.toString('hex')
-    +"\n\tIV: "+iv.toString('hex')
-    +"\n\tSignature: "+signature.toString('hex'));
-  //you had the salt, signature, and just got the server seed
-  const cipher = aes256(serverSeed, salt, iv);
-  //set up hash/hmac
-  const hash = crypto.createHash("sha256")
+const verify = (serverSeed, key, iv, salt, signature) => {
+  //you had the iv, key & signature 
+  //you just got the server seed & salt
+  const cipher = aes256(serverSeed, key, iv);
+  //set up hmac
   const hmac = crypto.createHmac("sha256", salt);
   const bets = hmac.update(cipher).digest('hex');
+
+  const hash = crypto.createHash("sha256");
+  //generate signature the same way the server did
   const sig = hash.update(bets).digest('hex');
-  console.log("We generated:\n\tSignature: "+sig);
+  console.log("We generated:\n\tSignature: " + sig);
+  //compare generated and give signatures
   return sig === signature;
 }
 
-const GameInit = (salt = null, iv = null) => {
+const GameInit = (key = null, iv = null) => {
+  //server seed & salt would be private. The client would have iv & key & the signature
   this.serverSeed = genServerSeed();
-  this.salt = salt || crypto.randomBytes(32);
+  this.salt = crypto.randomBytes(32);
+  //key should be generated using a safer method than this using a password hashing func
+  //tht works on user input 
+  this.key = key || crypto.randomBytes(32);
   this.iv = iv || crypto.randomBytes(16);
-  this.betString = genBetSig(this.serverSeed, this.salt, this.iv);
+  this.betString = genBetSig(this.serverSeed, this.key, this.iv, this.salt);
   this.wins = 0;
   this.loses = 0;
   this.bets = [];
 
+  console.log("Server gave signature: " + this.betString.sig.toString('hex'))
   if(!this.salt || !this.iv || !this.serverSeed || !this.betString) 
     throw new Error("not initialized...");
   
@@ -85,28 +81,44 @@ const GameInit = (salt = null, iv = null) => {
     for(let b = index; b < this.betString.bets.length; b += 5) {
       const stance = Math.floor(Math.random() * 2); //[0, 1]
       let win = placeBet(stance, this.betString.bets.substring(b, b + 5));
-      this.bets.push({stance: stance == HEADS ? "heads" : "tails", win: win.bet, outcome: win.roll});
-      win.bet === true ? this.cwin++ : this.lwin++;
+      this.bets.push({stance: stance == HEADS ? "heads" : "tails", win: win.bet, outcome: win.roll, hex: win.hex});
+      win.bet === true ? this.wins++ : this.loses++;
     }
-    console.log("All bets:\n", this.bets)
-    console.log("Done betting, wins: ", this.cwin, " Loses: ", this.lwin)
-    var check = verify(this.serverSeed, this.salt, this.iv, this.betString.sig);
-    console.log("The server generated bets (truthfully) random: "+check);
+    this.verify();
     process.exit(0);
   };
   
   this.place = (stance) => {
     //place bet
     const index = this.bets.length * 5;
-    if(index >= MAXBETS) 
-      return GameInit().place(stance);
+    if(this.bets.length >= MAXBETS) {
+      this.verify();
+      return GameInit().place(stance);  //new instance
+    }
     else {
       let win = placeBet(stance, this.betString.bets.substring(index, index + 5));
-      this.bets.push({stance: stance == HEADS ? "heads" : "tails", win: win.bet, outcome: win.roll})
-      win.bet === true ? this.cwin++ : this.lwin++;
+      this.bets.push({stance: stance == HEADS ? "heads" : "tails", win: win.bet, outcome: win.roll, hex: win.hex})
+      win.bet === true ? this.wins++ : this.loses++;
     }
+
     return this;
   };
+
+  this.verify = () => {
+    console.log("Verifying:"+
+    "\n\tServer Seed: "+this.serverSeed.toString('hex')
+    +"\n\tSalt: "+this.salt.toString('hex')
+    +"\n\tIV: "+this.iv.toString('hex')
+    +"\n\tSignature: "+this.betString.sig.toString('hex'));
+
+    const check = verify(this.serverSeed, this.key, this.iv, this.salt, this.betString.sig);
+    
+    console.log("All bets:\n", this.bets)
+    console.log("Wins: ", this.wins, " Loses: ", this.loses);
+    console.log("Was the server honest? "+(check === true ? "yes" : "no"));
+    return;
+  }
+
   return this;
 }
 
