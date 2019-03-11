d58165fe61f485c0eb20514a06eea7424ab80a1e
danilovlad <danilo.vladicic@gmail.com>
2019-03-11 18:25:21 -0400

    Cleaning up...

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readln = require("readline");
 var { GameInit, HEADS, TAILS } = require("./coin");
 
-var x = GameInit();
+var game = GameInit();
 var balance = 0;
 var wins = 0, loses = 0;
 
@@ -22,48 +22,48 @@ rl.on('line', (answer) => {
     console.log(`Balance: ${balance}`);
 
     if(!isNaN(balance))
-      rl.setPrompt("Enter Heads or Tails and a bet amount (ie: Heads 10)> ");
+      rl.setPrompt("Enter Heads (under 49.50) or Tails (over 50.50) and a bet amount (ie: Heads 10)> ");
     else 
       balance = 0;
   }
   else {
     if(answer == 'run')
-      return x.run();
-
-    answer = answer.trim().split(" ");
-    const stance = answer[0].toLowerCase();
-    const amount = parseFloat(answer[1],10);
-
-    if(amount <= balance && cmds.includes(stance)) {
-      //handle inputs
-      switch(stance) {
-        case 'quit': 
-          break;
-        case 'tails': 
-          x = x.place(TAILS); 
-          break;
-        case 'heads': 
-          x = x.place(HEADS); 
-          ;
+      return game.run();
+    else if(answer == 'quit') {
+      return;
+    }
+    else {
+      answer = answer.trim().split(" ");
+      const stance = answer[0].toLowerCase();
+      const amount = parseFloat(answer[1], 10);
+      if(amount > balance || isNaN(amount)) {
+        console.log("Invalid amount!");
+        return;
       }
-      const result = x.bets[0 || x.bets.length-1]
+
+      stance === 'tails' ? game = game.place(TAILS) : game = game.place(HEADS);
+
+      const result = game.bets[0 || game.bets.length - 1]
+
       if(result.win) {
         wins++;
         balance += amount;
+        console.log("WON: " + amount);
       }
       else {
         loses++;
         balance -= amount;
+        console.log("LOST: " + amount);
+        balance === 0 ? r1.setPrompt("Initial Balance >") : 0 ;
       }
-      if(balance == 0) 
-        rl.setPrompt("Initial Balance > ");
-
-      console.log(`Position: ${stance == 'tails' ? 'over 50.50/tails'  : 'under 49.50/heads'}, Amount: ${amount}, Result: ${result.win ? 'win' : 'loss', result.outcome}`)
-      console.log(`Wins: ${wins}, Loses: ${loses}, Balance: ${balance}`)
-
-    } else console.log("Not enough balance...")
+    
+      
+      console.log("Position: " + stance + " | Roll: " + result.outcome + " 0x" + result.hex.toUpperCase());
+      console.log("Wins: " + wins + " Loses: " + loses);
+      console.log("Balance: "+balance);
+    }
   }
   rl.prompt();
 });
 //run verify
-rl.on('close', () => console.log(''))
+rl.on('close', () => console.log("\nThanks for playing!"))
